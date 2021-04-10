48a0ccc3cdcc4a3540e077910d83cd94555a9656
Vivian Li <vdl21@cam.ac.uk>
2021-04-10 22:52:45 +0100

    Added other operators and the ability to choose

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const readline = require('readline-sync');
 console.log('Welcome to the calculator!');
-console.log('Please enter a number to multiply:');
-const num1 = readline.prompt();
-console.log('Please enter a second number to multiply by:');
-const num2 = readline.prompt();
-var product = (+num1) * (+num2);
-if (isNaN(product)) {
-    console.log('Error: your inputs were not valid and cannot be multiplied.');
-} else {   
-    console.log(`The product of ${num1} and ${num2} is ${product}.`);
+console.log('===================================');
+
+console.log('Please enter the operator: ');
+const op = readline.prompt();
+console.log('Please enter the first number: ');
+const prompt1 = readline.prompt();
+const num1 = (+prompt1);
+
+console.log('Please enter the second number: ');
+const prompt2 = readline.prompt();
+const num2 = (+prompt2);
+
+switch (op) {
+    case "+":
+        console.log(`The sum of ${num1} and ${num2} is ${num1 + num2}.`);
+        break;
+    case "-":
+        console.log(`The difference between ${num1} and ${num2} is ${num1 - num2}.`);
+        break;
+    case "*":
+        console.log(`The product of ${num1} and ${num2} is ${num1 * num2}.`);
+        break;
+    case "/":
+        console.log(`The quotient of ${num1} and ${num2} is ${num1 / num2}.`);
+        break;
+    default:
+        console.log('The specified operator is not a valid one.');
 }
