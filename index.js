6d473203121eba63f7fad8a254838e6c47711d14
Vivian Li <vdl21@cam.ac.uk>
2021-04-12 12:16:26 +0100

    Adding multiple numbers

--- index.js
+++ index.js
@@ -4,26 +4,44 @@ console.log('===================================');
 
 console.log('Please enter the operator: ');
 const op = readline.prompt();
-console.log('Please enter the first number: ');
-const prompt1 = readline.prompt();
-const num1 = (+prompt1);
 
-console.log('Please enter the second number: ');
-const prompt2 = readline.prompt();
-const num2 = (+prompt2);
+console.log(`How many numbers do you want to ${op}? `);
+const iters = readline.prompt();
+const it = (+iters);
+
+const numArray = new Array(it);
+for (i = 0; i < it; i++){
+    console.log(`Please enter number ${i + 1}: `);
+    const prompt = readline.prompt();
+    numArray[i] = (+prompt);
+}
+
+var total = numArray[0];
 
 switch (op) {
     case "+":
-        console.log(`The sum of ${num1} and ${num2} is ${num1 + num2}.`);
+        for (i = 1; i < numArray.length; i++) {
+            total += numArray[i];
+        }
+        console.log(`The sum of ${numArray.join(' + ')} is ${total}.`);
         break;
     case "-":
-        console.log(`The difference between ${num1} and ${num2} is ${num1 - num2}.`);
+        for (i = 1; i < numArray.length; i++) {
+            total -= numArray[i];
+        }
+        console.log(`The difference of ${numArray.join(' - ')} is ${total}.`);
         break;
     case "*":
-        console.log(`The product of ${num1} and ${num2} is ${num1 * num2}.`);
+        for (i = 1; i < numArray.length; i++) {
+            total *= numArray[i];
+        }
+        console.log(`The product of ${numArray.join(' * ')} is ${total}.`);
         break;
     case "/":
-        console.log(`The quotient of ${num1} and ${num2} is ${num1 / num2}.`);
+        for (i = 1; i < numArray.length; i++) {
+            total /= numArray[i];
+        }
+        console.log(`The quotient of ${numArray.join(' / ')} is ${total}.`);
         break;
     default:
         console.log('The specified operator is not a valid one.');
