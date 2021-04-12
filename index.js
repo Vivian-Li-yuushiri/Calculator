be81e62a3b4c2213849dc8da3d33ad1b28876579
Vivian Li <vdl21@cam.ac.uk>
2021-04-12 15:48:26 +0100

    Reformatted as functions

--- index.js
+++ index.js
@@ -1,48 +1,69 @@
 const readline = require('readline-sync');
-console.log('Welcome to the calculator!');
-console.log('===================================');
 
-console.log('Please enter the operator: ');
-const op = readline.prompt();
+function printWelcomeMessage() {
+    console.log('Welcome to the calculator!');
+    console.log('===================================');
+}
+function runCalculateLoop() {
+    function askOperator() {
+        console.log('Please enter the operator: ');
+        var op = readline.prompt();
+        while (!['+', '-', '*', '/'].includes(op)) {
+            console.log('That is not a valid operator. This calculator supports +, -, *, / as operators: ');
+            op = readline.prompt();
+        }
+        return op;
+    }
+    function askNumber(prompt) {
+        console.log(prompt);
+        var response = readline.prompt();
+        var num = (+response);
+        while (isNaN(num)) {
+            console.log('That is not a number. Please enter a number: ');
+            response = readline.prompt();
+            num = (+response);
+        }
+        return num;
+    }
+    function getNumArray(iters) {
+        const numArray = new Array(iters);
+        for (i = 0; i < iters; i++) {
+            numArray[i] = askNumber(`Please enter number ${i + 1}: `);
+        }
+        return numArray;
+    }
+    function runCalculations(op, numArray) {
+        var total = numArray[0];
 
-console.log(`How many numbers do you want to ${op}? `);
-const iters = readline.prompt();
-const it = (+iters);
-
-const numArray = new Array(it);
-for (i = 0; i < it; i++){
-    console.log(`Please enter number ${i + 1}: `);
-    const prompt = readline.prompt();
-    numArray[i] = (+prompt);
+        for (i = 1; i < numArray.length; i++){
+            switch (op) {
+                case "+":
+                    total += numArray[i];
+                    break;
+                case "-":
+                    total -= numArray[i];
+                    break;
+                case "*":
+                    total *= numArray[i];
+                    break;
+                case "/":
+                    total /= numArray[i];
+                    break;
+                default:
+                    console.log('Something went wrong and the specified operator is not recognized.');
+            }
+        }
+        return total;
+    }
+    
+    const op = askOperator();
+    const iters = askNumber(`How many numbers do you want to ${op}? `);
+    const numArray = getNumArray(iters);
+    const total = runCalculations(op, numArray);
+    console.log(`Your calculation ${numArray.join(' ' + op + ' ')} = ${total}.`);
 }
 
-var total = numArray[0];
-
-switch (op) {
-    case "+":
-        for (i = 1; i < numArray.length; i++) {
-            total += numArray[i];
-        }
-        console.log(`The sum of ${numArray.join(' + ')} is ${total}.`);
-        break;
-    case "-":
-        for (i = 1; i < numArray.length; i++) {
-            total -= numArray[i];
-        }
-        console.log(`The difference of ${numArray.join(' - ')} is ${total}.`);
-        break;
-    case "*":
-        for (i = 1; i < numArray.length; i++) {
-            total *= numArray[i];
-        }
-        console.log(`The product of ${numArray.join(' * ')} is ${total}.`);
-        break;
-    case "/":
-        for (i = 1; i < numArray.length; i++) {
-            total /= numArray[i];
-        }
-        console.log(`The quotient of ${numArray.join(' / ')} is ${total}.`);
-        break;
-    default:
-        console.log('The specified operator is not a valid one.');
+printWelcomeMessage();
+while (true) {
+    runCalculateLoop();
 }
